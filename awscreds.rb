eebf932e1b1c35d1fa773c3d67d9488a299c307b
Jacob Fleming-Gale <jacob.fleming-gale@domain.com.au>
2020-02-29 19:28:46 +1100

    Adding awcreds

---
+++ awscreds.rb
@@ -0,0 +1,44 @@
+class Awscreds < Formula
+  include Language::Python::Virtualenv
+
+  homepage 'https://github.com/jacobfg/awscreds'
+  version '4'
+
+  desc "Python-based, generic static web site generator aimed at developers"
+  # add :revision or tag ?
+  url "git@github.com:jacobfg/awscreds.git", :using => :git
+  # sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  head "git@github.com:jacobfg/awscreds.git", :using => :git, :branch => "develop"
+
+  # TODO: If you're submitting an existing package, make sure you include your
+  #       bottle block here.
+
+  depends_on 'python'
+  depends_on 'saml2aws'
+ 
+  def install
+    venv = virtualenv_create(libexec, "python3")
+    system libexec/"bin/pip", "install", "-v", "-r", "requirements.txt",
+                              "--ignore-installed", buildpath
+    system libexec/"bin/pip", "uninstall", "-y", "awscreds"
+    venv.pip_install_and_link buildpath
+    # pkgshare.install "awscli/examples"
+ 
+  end
+
+  # TODO: Add your package's tests here
+  test do
+    system "#{bin}/awscreds"
+    # assert_match "topics", shell_output("#{bin}/aws help")
+  end
+
+  # def caveats; <<~EOS
+  #   The "examples" directory has been installed to:
+  #     #{HOMEBREW_PREFIX}/share/awscli/examples
+  # EOS
+  # end
+
+  # test do
+  #   assert_match "topics", shell_output("#{bin}/aws help")
+  # end
+end
