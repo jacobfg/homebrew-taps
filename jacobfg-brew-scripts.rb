0353c17ae97fb8e64b5c1846c82891bc00c496df
Jacob Fleming-Gale <jacob.fleming-gale@domain.com.au>
2020-03-20 15:04:55 +1100

    Fix brew purge iissues with multiple packages

--- jacobfg-brew-scripts.rb
+++ jacobfg-brew-scripts.rb
@@ -3,12 +3,12 @@ require_relative './lib/download_strategy.rb'
 class JacobfgBrewScripts < Formula
 
   homepage "https://gist.github.com/jacobfg/727e697833ac570cdac25da98490832f"
-  version "2"
+  version "3"
 
   desc "Additional homebrew scripts"
-  url "https://gist.github.com/jacobfg/727e697833ac570cdac25da98490832f/archive/9280aa6ae799ae7d336e5ea2fc49d5d8cf4c68da.zip"
+  url "https://gist.github.com/jacobfg/727e697833ac570cdac25da98490832f/archive/ed737b29ebc6b4d61da14caa59f1912913e553f7.zip"
   # , :using => GitHubPrivateGistDownloadStrategy
-  sha256 "52f12546ea7f7823167f8a573de2c8420152219c5b797f3ac8f5234213bd807c"
+  sha256 "867e56cf2891b29db15cee9247e363eb7e6cd12ea55b33f4483701a33b7c7a8f"
 
   def install
     bin.install "brew-purge"
