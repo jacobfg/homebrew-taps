b992d20a5eafebfc0f73b466ea9afad864974806
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2020-03-27 15:04:50 +1100

    Testing ...

--- jacobfg-laptop-domain.rb
+++ jacobfg-laptop-domain.rb
@@ -9,7 +9,7 @@ class JacobfgLaptopDomain < Formula
 
   if OS.mac?
     url "file:///dev/null", :using => NullDownloadStrategy
-    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    sha256 "84ff92691f909a05b224e1c56abb4864f01b4f8e3c854e4bb4c7baf1d3f6d652"
   # elsif OS.linux?
   #   url "file:///dev/null"
   #   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
