1b391f3bd8b8fa60c1f6d78cbc18eb9da3e34a61
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2020-03-27 14:19:36 +1100

    Adding stub package for laptop

---
+++ jacobfg-laptop-domain.rb
@@ -0,0 +1,31 @@
+class JacobfgLaptopDomain < Formula
+  include Language::Python::Virtualenv
+
+  homepage 'https://github.com/jacobfg/homebrew-taps'
+  version '0.0.1'
+  desc "Stub package to pull in my other packages"
+
+  if OS.mac?
+    url "file:///dev/null"
+    sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  # elsif OS.linux?
+  #   url "file:///dev/null"
+  #   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  end
+
+  depends_on :arch => :x86_64
+
+  depends_on 'jacobfg-awscreds'
+  depends_on 'jacobfg-brew-scripts'
+  depends_on 'jacobfg-gpg-scripts'
+  depends_on 'jacobfg-python-scripts'
+
+  def install
+    # bin.install "gpg-switch-card"
+  end
+
+  test do
+    # system "#{bin}/gpg-switch-card"
+  end
+
+end
