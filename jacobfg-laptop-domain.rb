cbf76bf8ac1867b5c81994886350f81b630acde6
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2020-03-27 17:14:00 +1100

    Testing ...

--- jacobfg-laptop-domain.rb
+++ jacobfg-laptop-domain.rb
@@ -8,6 +8,8 @@ class JacobfgLaptopDomain < Formula
 
   url "empty", :using => NullDownloadStrategy
   # sha256 "84ff92691f909a05b224e1c56abb4864f01b4f8e3c854e4bb4c7baf1d3f6d652"
+
+  # sha256 "7bbe1be53b7942f0baac7dcdae081b5326d5251926320a7ae217a8dea7157388"
  
   depends_on :arch => :x86_64
 
