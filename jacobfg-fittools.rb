e9311763a56788e6f392b8e0c643313c25303ace
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2020-07-14 18:06:12 +1000

    Uplift fittools

--- jacobfg-fittools.rb
+++ jacobfg-fittools.rb
@@ -3,8 +3,8 @@ require_relative './lib/download_release_strategy.rb'
 class JacobfgFittools < Formula
     desc "FIT Tools - java wrangler for Garmin/ANT FIT files"
     homepage "https://github.com/jacobfg/java-fitfile-tool"
-    url "https://github.com/jacobfg/java-fitfile-tool/releases/download/0.0.2/fittools-0.0.2.jar", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-    sha256 "31faf082d921d3b5c891d2938cd04168db3268a74a81da391a89f1984f9d387e"
+    url "https://github.com/jacobfg/java-fitfile-tool/releases/download/0.0.3/fittools-0.0.3.jar", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    sha256 "8e40a0e969d07e5472bb71a51ca9edd1b7aafb6b774b0bb4022578a1ea5a44d0"
     head "https://github.com/jacobfg/java-fitfile-tool"
   
     bottle :unneeded
