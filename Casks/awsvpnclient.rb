cc44cc08b9075b3de66efdb25635d89e01319bfb
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2020-08-14 09:08:11 +1000

    Removing hash for vpn client

--- Casks/awsvpnclient.rb
+++ Casks/awsvpnclient.rb
@@ -1,6 +1,6 @@
 cask 'awsvpnclient' do
   version 'latest'
-  sha256 'ab9d1d742e2e490e31d67463115c6923322b83ede65d184b064d2eeb93af0e2c'
+#  sha256 'ab9d1d742e2e490e31d67463115c6923322b83ede65d184b064d2eeb93af0e2c'
   url 'https://d20adtppz83p9s.cloudfront.net/OSX/latest/AWS_VPN_Client.pkg'
 #  appcast 'https://github.com/users/appname/releases.atom'
   name 'AWS VPN Client'
