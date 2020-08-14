dad384a31af691756ca8484aacad8da38d1f1f74
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2020-08-14 13:15:25 +1000

    Fixing checksum for aws vpn

--- Casks/awsvpnclient.rb
+++ Casks/awsvpnclient.rb
@@ -1,6 +1,7 @@
 cask 'awsvpnclient' do
-  version 'latest'
-#  sha256 'ab9d1d742e2e490e31d67463115c6923322b83ede65d184b064d2eeb93af0e2c'
+  version :latest
+  sha256 :no_check
+
   url 'https://d20adtppz83p9s.cloudfront.net/OSX/latest/AWS_VPN_Client.pkg'
 #  appcast 'https://github.com/users/appname/releases.atom'
   name 'AWS VPN Client'
