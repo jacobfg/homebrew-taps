f4aef8b1866eb4fc13dc9095329f67059a02a98a
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2020-08-25 00:25:47 +1000

    Update AWS ssm plugin url

--- aws-session-manager-plugin.rb
+++ aws-session-manager-plugin.rb
@@ -1,7 +1,7 @@
 class AwsSessionManagerPlugin < Formula
   desc "Official Amazon AWS session manager plugin"
   homepage "https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html"
-  url "https://s3.amazonaws.com/session-manager-downloads/plugin/1.1.61.0/mac/sessionmanager-bundle.zip"
+  url "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac/sessionmanager-bundle.zip"
 
   version "1.1.61.0"
   sha256 "426615b12b2d7728504ee7896c60fa330534314d6cb64ee8cb3ea0488e7d66e4"
