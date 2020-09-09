56392f8c9015473a17f71b4d11c74e934f8cf298
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2020-09-09 21:03:51 +1000

    Update jacobfg-bash-scripts

--- jacobfg-bash-scripts.rb
+++ jacobfg-bash-scripts.rb
@@ -3,11 +3,11 @@ require_relative './lib/download_strategy.rb'
 class JacobfgBashScripts < Formula
 
   homepage "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45"
-  version "1"
+  version "1.0.1"
 
   desc "Bash scripts"
-  url "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45/archive/fc695f2cbd25c0ee01cd8f6a742e0de70a92e759.zip", :using => GitHubPrivateGistDownloadStrategy
-  sha256 "874de1eb516f791128765c56a75949ce738aaeae7c245eb8f7264542b04f9706"
+  url "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45/archive/92cee13507702586f6dbbb1597dc92353e199f27.zip", :using => GitHubPrivateGistDownloadStrategy
+  sha256 "862291ccc2d864bf04e27c3ec210397dd18fc864d3b8868ad1dafaf322f3d1e9"
 
   def install
     bin.install "brew-backup"
