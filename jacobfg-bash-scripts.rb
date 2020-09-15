be0d2013d87de2042329fe45bd7c39817bd53c5e
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2020-09-15 22:29:01 +1000

    updating jacobfg-bash-scripts

--- jacobfg-bash-scripts.rb
+++ jacobfg-bash-scripts.rb
@@ -3,17 +3,18 @@ require_relative './lib/download_strategy.rb'
 class JacobfgBashScripts < Formula
 
   homepage "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45"
-  version "1.0.2"
+  version "1.0.3"
 
   desc "Bash scripts"
-  url "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45/archive/90bc6df558dcdef30b1fa2b79d13400d94aa32be.zip", :using => GitHubPrivateGistDownloadStrategy
-  sha256 "0a9d55f2bc4a318d3fde985520fa0a1c77c3fee6aaa58b179c3ebeddd9e60d8e"
+  url "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45/archive/9442e9fecb024d5e42a7a0f8afbc56076ed8584f.zip", :using => GitHubPrivateGistDownloadStrategy
+  sha256 "94f3d92b73fddcbe09f15e9800e916b90e22cf86b3fa26b7bb8471bccc014b39"
 
   def install
     bin.install "brew-backup"
     bin.install "brew-latest"
     bin.install "github-head-sha256"
     bin.install "login-cleanup"
+    bin.install "usb-backup"
   end
 
   test do
@@ -21,6 +22,7 @@ class JacobfgBashScripts < Formula
     system "#{bin}/brew-latest"
     system "#{bin}/github-head-sha256"
     system "#{bin}/login-cleanup"
+    system "#{bin}/usb-backup"
   end
 
 end
