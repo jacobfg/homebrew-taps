76643187af9e34e755febaf8d37b35e9f08464d4
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2020-09-16 21:44:40 +1000

    adding missing jsondiff

--- jacobfg-bash-scripts.rb
+++ jacobfg-bash-scripts.rb
@@ -3,7 +3,7 @@ require_relative './lib/download_strategy.rb'
 class JacobfgBashScripts < Formula
 
   homepage "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45"
-  version "1.0.5"
+  version "1.0.6"
 
   desc "Bash scripts"
   url "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45/archive/e39756cce834ac582c0d1f054f32d0ffcbdc0b42.zip", :using => GitHubPrivateGistDownloadStrategy
@@ -12,6 +12,7 @@ class JacobfgBashScripts < Formula
   def install
     bin.install "brew-backup"
     bin.install "brew-latest"
+    bin.install "jsondiff"
     bin.install "github-head-sha256"
     bin.install "login-cleanup"
     bin.install "usb-backup"
@@ -20,6 +21,7 @@ class JacobfgBashScripts < Formula
   test do
     system "#{bin}/brew-backup"
     system "#{bin}/brew-latest"
+    system "#{bin}/jsondiff"
     system "#{bin}/github-head-sha256"
     system "#{bin}/login-cleanup"
     system "#{bin}/usb-backup"
