7f1c063d8278dec8e478b7bc4fe40de3d656dbad
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2020-12-14 10:52:18 +1100

    Updating AWS VPN Client

--- Casks/awsvpnclient.rb
+++ Casks/awsvpnclient.rb
@@ -1,30 +1,21 @@
-cask 'awsvpnclient' do
+# https://formulae.brew.sh/cask/aws-vpn-client
+# doesn't include zap
+
+cask "awsvpnclient" do
   version :latest
   sha256 :no_check
 
-  url 'https://d20adtppz83p9s.cloudfront.net/OSX/latest/AWS_VPN_Client.pkg'
-#  appcast 'https://github.com/users/appname/releases.atom'
-  name 'AWS VPN Client'
-  homepage 'https://aws.amazon.com/vpn/client-vpn-download/'
+  url "https://d20adtppz83p9s.cloudfront.net/OSX/latest/AWS_VPN_Client.pkg",
+      verified: "d20adtppz83p9s.cloudfront.net/"
+  name "AWS VPN Client"
+  homepage "https://aws.amazon.com/vpn/client-vpn-download/"
 
-  pkg 'AWS_VPN_Client.pkg'
+  pkg "AWS_VPN_Client.pkg"
 
-  uninstall quit:     [
-                        'AWS VPN Client.app',
-                        # 'com.amazonaws.acvc',
-                        'com.amazonaws.acvc.helper'
-                      ],
+  uninstall quit:      "AWS VPN Client.app",
+            pkgutil:   "com.amazon.awsvpnclient",
+            launchctl: "com.amazonaws.acvc.helper"
 
-            pkgutil:  [
-                        'com.amazon.awsvpnclient',
-                      ]
-
-  zap delete:         [
-                        '/Library/Application Support/AWSVPNClient',
-                      ]
-
-  zap trash:          [
-                        '~/.config/AWSVPNClient',
-                      ]
-
+  zap delete: "/Library/Application Support/AWSVPNClient",
+      trash:  "~/.config/AWSVPNClient"
 end
