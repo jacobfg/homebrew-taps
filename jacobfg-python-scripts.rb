1cd55629338f76583e3b0cebb14095da772d4346
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2021-01-11 11:52:43 +1100

    new versions

--- jacobfg-python-scripts.rb
+++ jacobfg-python-scripts.rb
@@ -8,7 +8,7 @@ class JacobfgPythonScripts < Formula
 
   desc "Python-based, generic static web site generator aimed at developers"
   url "https://github.com/jacobfg/python-scripts/archive/0.0.8.zip", :using => GitHubPrivateRepositoryDownloadStrategy
-  sha256 "31591c41793daaaa38d99e86fe50ae2ab64bfbed32426429b82eef71a2da10b2"
+  sha256 "ae3042a955278f8477f188854be49a864e5bf23bcd25d71a0fc65dd4677dd63f"
   head "https://github.com/jacobfg/python-scripts/archive/master.zip", :using => GitHubPrivateRepositoryDownloadStrategy
 
   # TODO: If you're submitting an existing package, make sure you include your
