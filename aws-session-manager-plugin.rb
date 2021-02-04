c2d4133b0dbb5337dc6b868f4baee9358fd294f5
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2021-02-04 14:24:20 +1100

    Lift aws-session-manager-plugin version

--- aws-session-manager-plugin.rb
+++ aws-session-manager-plugin.rb
@@ -3,8 +3,8 @@ class AwsSessionManagerPlugin < Formula
   homepage "https://docs.aws.amazon.com/systems-manager/latest/userguide/session-manager-working-with-install-plugin.html"
   url "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac/sessionmanager-bundle.zip"
 
-  version "1.1.61.12"
-  sha256 "426615b12b2d7728504ee7896c60fa330534314d6cb64ee8cb3ea0488e7d66e4"
+  version "1.2.54.0"
+  sha256 "920c6842bb6edadb15e6dff7a00f99da3b98db884f3b39cf1a395032ca8e2198"
 
   depends_on "awscli"
 
