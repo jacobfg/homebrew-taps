fc961b489b59e128252cd1b0a6bea30ee903363e
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2021-02-07 18:46:49 +1100

    rogue killer

--- bash-scripts.rb
+++ bash-scripts.rb
@@ -3,11 +3,11 @@ require_relative './lib/download_strategy.rb'
 class BashScripts < Formula
 
   homepage "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45"
-  version "1.0.9"
+  version "1.0.10"
 
   desc "Bash scripts"
-  url "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45/archive/894bd49124bdc19a3d39bb146a751ebbc468ba41.zip", :using => GitHubPrivateGistDownloadStrategy
-  sha256 "29799c766e8f8a95de7656baa307183c44bb2793355786d0d64774dcdb840f17"
+  url "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45/archive/23379769943ada53c07753cd4db52f7c378ea1d4.zip", :using => GitHubPrivateGistDownloadStrategy
+  sha256 "8729d4bf4c6026c4c2d109b051e873d972afc1a2afef239195355d072c813fb0"
 
   depends_on "pinentry-mac"
 
---
+++ kill-rogue-pinentry.rb
@@ -0,0 +1,54 @@
+require_relative './lib/download_strategy.rb'
+
+class KillRoguePinentry < Formula
+
+  homepage "https://gist.github.com/jacobfg/417e745354ac069af3e402c740ba3349"
+  version "0.0.1"
+
+  desc "Kill Rogue pinentry processes"
+  url "empty", :using => NullDownloadStrategy
+
+  depends_on "bash-scripts"
+
+  def install
+    (bin+"_kill-rogue-pinentry").write <<~EOS
+        #!/bin/sh 
+        echo launchctl to kill rogue pinentry process
+    EOS
+  end
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>Program</key>
+          <string>#{HOMEBREW_PREFIX}/bin/kill-long-process</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>top</string>
+            <string>12</string>
+          </array>
+          <key>EnvironmentVariables</key>
+          <dict>
+            <key>PATH</key>
+            <string>/bin:/usr/bin:/usr/local/bin</string>
+          </dict>
+          <key>StartInterval</key>
+          <integer>12</integer>
+        </dict>
+      </plist>
+    EOS
+  end
+
+  def caveats
+    <<~EOS
+      launchctl load #{bin}/../#{plist_name}.plist
+      launchctl start #{plist_name}
+    EOS
+  end
+
+end
