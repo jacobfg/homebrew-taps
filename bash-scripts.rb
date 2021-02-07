529dd29fd566c140213a6d38bd90a6db37013358
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2021-02-07 19:35:28 +1100

    uplift bash-scripts

--- bash-scripts.rb
+++ bash-scripts.rb
@@ -3,13 +3,14 @@ require_relative './lib/download_strategy.rb'
 class BashScripts < Formula
 
   homepage "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45"
-  version "1.0.10"
+  version "1.0.11"
 
   desc "Bash scripts"
-  url "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45/archive/23379769943ada53c07753cd4db52f7c378ea1d4.zip", :using => GitHubPrivateGistDownloadStrategy
-  sha256 "8729d4bf4c6026c4c2d109b051e873d972afc1a2afef239195355d072c813fb0"
+  url "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45/archive/a061dfbdf7559e1a060cfbe52234bde9e55f511c.zip", :using => GitHubPrivateGistDownloadStrategy
+  sha256 "c1cfa75e863cf8db5c736b1e5e29852a37f7577453bdee281ea98c836d2105a8"
 
   depends_on "pinentry-mac"
+  depends_on "terminal-notifier"
 
   def install
     bin.install "brew-backup"
