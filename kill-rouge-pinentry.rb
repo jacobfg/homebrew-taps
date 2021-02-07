109238fd3d41ecec3afa00bd32b9fd55a1d5e654
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2021-02-07 17:20:21 +1100

    Uplift kill-rogue-pinentry

--- kill-rouge-pinentry.rb
+++ kill-rouge-pinentry.rb
@@ -3,11 +3,11 @@ require_relative './lib/download_strategy.rb'
 class KillRougePinentry < Formula
 
   homepage "https://gist.github.com/jacobfg/417e745354ac069af3e402c740ba3349"
-  version "0.0.1"
+  version "0.0.2"
 
   desc "Kill Rouge pinentry processes"
-  url "https://gist.github.com/jacobfg/417e745354ac069af3e402c740ba3349/archive/d6ec874e36d34526ad91c9d968d3ef5e6378676b.zip", :using => GitHubPrivateGistDownloadStrategy
-  sha256 "7a9479ad1b04464ab19ae04c3cc3f9f595192c180fa6b4e50b4439748a8d8bb4"
+  url "https://gist.github.com/jacobfg/417e745354ac069af3e402c740ba3349/archive/5ac02d733d993ffacbd1d3d41b678919eab63919.zip", :using => GitHubPrivateGistDownloadStrategy
+  sha256 "5471563985a876594ec279a1f1edbadf740b79f6b6c70101b3fa56fb1b88dd29  "
 
   depends_on "pinentry-mac"
 
