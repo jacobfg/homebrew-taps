1de09dd12cc7fe4eacb18e94f741f336de71b427
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2021-02-07 17:31:41 +1100

    wip

--- kill-rogue-pinentry.rb
+++ kill-rogue-pinentry.rb
@@ -1,22 +1,22 @@
 require_relative './lib/download_strategy.rb'
 
-class KillRougePinentry < Formula
+class KillRoguePinentry < Formula
 
   homepage "https://gist.github.com/jacobfg/417e745354ac069af3e402c740ba3349"
   version "0.0.2"
 
-  desc "Kill Rouge pinentry processes"
-  url "https://gist.github.com/jacobfg/417e745354ac069af3e402c740ba3349/archive/5ac02d733d993ffacbd1d3d41b678919eab63919.zip", :using => GitHubPrivateGistDownloadStrategy
-  sha256 "5471563985a876594ec279a1f1edbadf740b79f6b6c70101b3fa56fb1b88dd29  "
+  desc "Kill Rogue pinentry processes"
+  url "https://gist.github.com/jacobfg/417e745354ac069af3e402c740ba3349/archive/a5f7d852e4362ffc98e6233998ccad7d14b72267.zip", :using => GitHubPrivateGistDownloadStrategy
+  sha256 "3605693e4d4e34efbbdfd83804b6554200728d2fde36a10ab31c4820834d3cd5"
 
   depends_on "pinentry-mac"
 
   def install
-    bin.install "kill-rouge-pinentry"
+    bin.install "kill-rogue-pinentry"
   end
 
   test do
-    system "#{bin}/kill-rouge-pinentry"
+    system "#{bin}/kill-rogue-pinentry"
   end
 
   def plist
@@ -28,7 +28,7 @@ class KillRougePinentry < Formula
           <key>Label</key>
           <string>#{plist_name}</string>
           <key>Program</key>
-          <string>#{opt_bin}/kill-rouge-pinentry</string>
+          <string>#{opt_bin}/kill-rogue-pinentry</string>
           <key>ProgramArguments</key>
           <array>
             <string>120</string>
