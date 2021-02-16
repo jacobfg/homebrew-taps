2286e2d034cc00f34a5f5694c4ee2035f56c78bf
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2021-02-16 22:19:02 +1100

    Adding global protect

--- bash-scripts.rb
+++ bash-scripts.rb
@@ -3,7 +3,7 @@ require_relative './lib/download_strategy.rb'
 class BashScripts < Formula
 
   homepage "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45"
-  version "1.0.12"
+  version "1.0.13"
 
   desc "Bash scripts"
   url "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45/archive/03b03c094aaf40ac107f9ff3f309619e19179193.zip", :using => GitHubPrivateGistDownloadStrategy
@@ -19,6 +19,7 @@ class BashScripts < Formula
     bin.install "github-head-sha256"
     bin.install "usb-backup"
     bin.install "kill-long-process"
+    bin.install "globalprotect"
   end
 
   test do
