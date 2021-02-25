cb65b8a58193b983b7009b607d59b63f759975a2
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2021-02-25 12:35:15 +1100

    cleanup after moving to gpg gui auth

--- bash-scripts.rb
+++ bash-scripts.rb
@@ -3,11 +3,11 @@ require_relative './lib/download_strategy.rb'
 class BashScripts < Formula
 
   homepage "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45"
-  version "1.0.17"
+  version "1.0.18"
 
   desc "Bash scripts"
-  url "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45/archive/2ec8b3dc95af2e67ee3d9bb00eba784a43bee65d.zip", :using => GitHubPrivateGistDownloadStrategy
-  sha256 "3369d087d64d99a9a20365657a662d7a944b96535c6fe5144bfe73db17d28f11"
+  url "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45/archive/e0b2633e2a40c83fb3d8056f5f02089b0d004836.zip", :using => GitHubPrivateGistDownloadStrategy
+  sha256 "ced441268cb5c864b3a48ad4325f65908d7215ef8b0ef8898102c2b5e44d28f4"
 
   depends_on "pinentry-mac"
   depends_on "terminal-notifier"
@@ -20,7 +20,6 @@ class BashScripts < Formula
     bin.install "usb-backup"
     bin.install "kill-long-process"
     bin.install "globalprotect"
-    bin.install "pinentry-auto"
   end
 
   test do
@@ -31,7 +30,6 @@ class BashScripts < Formula
     system "#{bin}/usb-backup"
     system "#{bin}/kill-long-process"
     system "#{bin}/globalprotect"
-    system "#{bin}/pinentry-auto"
   end
 
 end
