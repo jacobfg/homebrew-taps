d11cdb4ae01033286159a45d0ecacdeb2b43bac3
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2021-02-26 12:31:05 +1100

    Cleanup

--- bash-scripts.rb
+++ bash-scripts.rb
@@ -3,35 +3,27 @@ require_relative './lib/download_strategy.rb'
 class BashScripts < Formula
 
   homepage "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45"
-  version "1.0.19"
+  version "1.0.20"
 
   desc "Bash scripts"
-  url "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45/archive/401d025ad4a2b4e66df8ea372a81cd008a93e54f.zip", :using => GitHubPrivateGistDownloadStrategy
-  sha256 "06ec1fe794314e4ae2eceb60610d5ab0078d531e2367adff1a654a9b6b0f5a4b"
+  url "https://gist.github.com/jacobfg/5e507760cde9a1c7a21048f47db3af45/archive/74f88d81889274cc5f21f9618a54d3001af0929b.zip", :using => GitHubPrivateGistDownloadStrategy
+  sha256 "0de5ac9f310edaec06cba7589e5d4aa202796ea338f2d32e805e4f63795bdbfc"
 
   depends_on "pinentry-mac"
   depends_on "terminal-notifier"
 
   def install
-    bin.install "brew-backup"
     bin.install "brew-latest"
     bin.install "jsondiff"
     bin.install "github-head-sha256"
     bin.install "usb-backup"
-    bin.install "kill-long-process"
-    bin.install "globalprotect"
-    bin.install "macbook-vnc"
   end
 
   test do
-    system "#{bin}/brew-backup"
     system "#{bin}/brew-latest"
     system "#{bin}/jsondiff"
     system "#{bin}/github-head-sha256"
     system "#{bin}/usb-backup"
-    system "#{bin}/kill-long-process"
-    system "#{bin}/globalprotect"
-    system "#{bin}/macbook-vnc"
   end
 
 end
