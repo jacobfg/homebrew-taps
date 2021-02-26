b755494560de665f629d84e4cd6569fc35bdbc2b
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2021-02-26 23:31:03 +1100

    Uplift gpgscripts

--- gpg-scripts.rb
+++ gpg-scripts.rb
@@ -3,12 +3,12 @@ require_relative './lib/download_strategy.rb'
 class GpgScripts < Formula
 
   homepage "https://gist.github.com/jacobfg/2b7c86fbdbfdbbca271af9d295459433"
-  version "1"
+  version "2"
 
   desc "Yubikey GPG/SSH homebrew scripts"
-  url "https://gist.github.com/jacobfg/2b7c86fbdbfdbbca271af9d295459433/archive/147fb8b03bbcccf0ce3bfae9e24b8900a3da29de.zip"
+  url "https://gist.github.com/jacobfg/2b7c86fbdbfdbbca271af9d295459433/archive/9f9102563614861c570cc5602fe1b1387ab6b3c7.zip"
   # , :using => GitHubPrivateGistDownloadStrategy
-  sha256 "bbfb06d79a1701321cd485ff211aaa852d50bbe49b58132a5d14fcc3cf00e943"
+  sha256 "363f72f07c1e902504f6c0e46e707f6b4e7340d5364831dcaa92a0228ae09234"
 
   def install
     bin.install "gpg-switch-card"
