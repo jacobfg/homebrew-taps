bef9e81c95f20749a1a4a9f89496069e0ffc89cc
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2021-02-27 11:35:26 +1100

    Cleanup

--- python-scripts.rb
+++ python-scripts.rb
@@ -4,11 +4,11 @@ class PythonScripts < Formula
   include Language::Python::Virtualenv
 
   homepage 'https://github.com/jacobfg/python-scripts'
-  version '0.0.13'
+  version '0.0.14'
 
   desc "Python-based, generic static web site generator aimed at developers"
-  url "https://github.com/jacobfg/python-scripts/archive/0.0.13.zip", :using => GitHubPrivateRepositoryDownloadStrategy
-  sha256 "0ce46164c5b07c2838d438780367e23b46373770b2b1c9b12b55adf83d4590f2"
+  url "https://github.com/jacobfg/python-scripts/archive/0.0.14.zip", :using => GitHubPrivateRepositoryDownloadStrategy
+  sha256 "7e77b2b483c21ffc9ba7c82e16c9ca2f6d85d344fe6b4af27f092290aeac3be3"
   head "https://github.com/jacobfg/python-scripts/archive/main.zip", :using => GitHubPrivateRepositoryDownloadStrategy
 
   # TODO: If you're submitting an existing package, make sure you include your
@@ -28,18 +28,11 @@ class PythonScripts < Formula
   # TODO: Add your package's tests here
   test do
     system "#{bin}/cert-details"
-    system "#{bin}/gmail-filters"
     system "#{bin}/httpdecode"
     system "#{bin}/httpencode"
+    system "#{bin}/ini-print"
     system "#{bin}/totp"
-    system "#{bin}/pswrapper"
     # assert_match "topics", shell_output("#{bin}/aws help")
   end
-
-  # def caveats; <<~EOS
-  #   The "examples" directory has been installed to:
-  #     #{HOMEBREW_PREFIX}/share/awscli/examples
-  # EOS
-  # end
 
 end
