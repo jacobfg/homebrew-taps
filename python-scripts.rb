540e5d172844f96486b46f99f855e77049fc35ad
Jacob Fleming-Gale <10949267+jacobfg@users.noreply.github.com>
2022-02-23 21:38:32 +1100

    python scripts tweaks

--- python-scripts.rb
+++ python-scripts.rb
@@ -4,25 +4,22 @@ class PythonScripts < Formula
   include Language::Python::Virtualenv
 
   homepage 'https://github.com/jacobfg/python-scripts'
-  version '0.0.17'
+  version '0.0.19'
 
   desc "Python-based, generic static web site generator aimed at developers"
-  url "https://github.com/jacobfg/python-scripts/archive/0.0.17.zip", :using => GitHubPrivateRepositoryDownloadStrategy
-  sha256 "ce066b6368bec3c6f35a3afc098555db05c66dbfb51cb7b080fee30e1a512906"
+  url "https://github.com/jacobfg/python-scripts/archive/0.0.19.zip", :using => GitHubPrivateRepositoryDownloadStrategy
+  sha256 "7294d5d80d1d78101d88fe7fcb7977fcdd74584cefe1f759ecf55550ceffa335"
   head "https://github.com/jacobfg/python-scripts/archive/main.zip", :using => GitHubPrivateRepositoryDownloadStrategy
 
   # TODO: If you're submitting an existing package, make sure you include your
   #       bottle block here.
 
-  depends_on 'python@3'
+  depends_on 'python@3.9'
  
   def install
-    venv = virtualenv_create(libexec, "python3")
-    system libexec/"bin/pip", "install", "-v", "-r", "requirements.txt",
-                              "--ignore-installed", buildpath
-    system libexec/"bin/pip", "uninstall", "-y", "python-scripts"
-    venv.pip_install_and_link buildpath
-    # pkgshare.install "awscli/examples"
+    virtualenv_install_with_resources
+    # HACK - need to define in resources section
+    system libexec/"bin/pip", "install", "-v", "-r", "requirements.txt", "--ignore-installed", buildpath
   end
 
   # TODO: Add your package's tests here
